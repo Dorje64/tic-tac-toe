753afc3bbe2e95f28f4a21ebf8ee0c1c7a48a76e
Dorje <ku.dorjee@gmail.com>
2017-11-07 14:49:10 +0545

    show step and travel back

--- src/index.js
+++ src/index.js
@@ -100,17 +100,36 @@ class Game extends React.Component {
     //setState is to call modify the state
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
+
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+    const desc = move ?
+      'Go to move #' + move :
+      'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
 
     return (
       <div className="game">
@@ -124,6 +143,7 @@ class Game extends React.Component {
 
         <div className="game-info">
           <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
